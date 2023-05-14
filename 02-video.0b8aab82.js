ed49915c8050a911636167b0fac5c2cf2d943cba
Dyhless <108591328+Dyhless@users.noreply.github.com>
2023-05-14 16:03:32 +0000

    Deploying to gh-pages from @ Dyhless/goit-js-hw-08@c07d5a2ea7eb1a1aee27038ce4fb2772095dc37d 🚀

--- 02-video.0b8aab82.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello"),localStorage.setItem("TEST_KEY","Hello wordl");
-//# sourceMappingURL=02-video.0b8aab82.js.map
--- 02-video.89bc6d58.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello"),localStorage.setItem("TEST_KEY","Hello wordl");
-//# sourceMappingURL=02-video.89bc6d58.js.map
