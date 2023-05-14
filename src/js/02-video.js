5d9677e7c0c3337e96041efd69a3e84ee169397f
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-14 18:42:06 +0200

    update 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
 
-const iframe = document.querySelector('#vimeo-player');
-    const player = new Vimeo.Player(iframe);
+// Обработчик события timeupdate, вызывается при обновлении времени воспроизведения
+const handleTimeUpdate = throttle(function (event) {
+  const currentTime = event.seconds; // Текущее время воспроизведения
+  localStorage.setItem('videoplayer-current-time', currentTime); // Сохраняем текущее время в локальное хранилище
+}, 1000); // Ограничиваем частоту вызова обработчика 1 раз в секунду
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+// Устанавливаем обработчик события timeupdate
+player.on('timeupdate', handleTimeUpdate);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+// При загрузке страницы восстанавливаем время воспроизведения, если оно сохранено в локальном хранилище
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(parseFloat(currentTime));
+}
