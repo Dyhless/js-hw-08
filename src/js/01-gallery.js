12bdc3c879b16ad8f987dfcfe27b0b36bc7b21e6
Dmytro Medvediev <dsisitkin@gmail.com>
2023-05-15 19:29:00 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -35,28 +35,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', // Используем атрибут alt для подписей
   captionDelay: 250, // Задержка перед отображением подписи
 });
-
-// Добавляем слушатель события клика на элементы галереи
-galleryList.addEventListener('click', onGalleryItem);
-
-// Обработчик события клика на элементе галереи
-function onGalleryItem(evt) {
-  evt.preventDefault();
-
-  // Получаем ссылку на элемент, по которому кликнули
-  const clickElement = evt.target;
-
-  // Проверяем, что кликнули именно по изображению
-  const isImage = clickElement.tagName === 'IMG';
-
-  if (!isImage) {
-    return;
-  }
-
-  // Получаем ссылку на оригинальное изображение из атрибута href
-  const originalUrl = clickElement.parentNode.getAttribute('href');
-
-  // Выводим ссылку на оригинальное изображение в консоль
-  console.log(originalUrl);
-}
-
